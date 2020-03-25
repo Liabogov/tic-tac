75327bb884feb7cdfeae4e045e9bded7c826e8be
Богдан Лябогов <bogdanlabogov@MacBook-Pro-Bogdan.local>
2020-03-25 09:58:41 +0200

    V4

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-      return (
-        <button className="square" onClick={props.onClick}>
-          {props.value}
-        </button>
-      );
-    }
+// function Square(props) {
+//       return (
+//         <button className="square" onClick={props.onClick}>
+//           {props.value}
+//         </button>
+//       );
+//     }
     
   
   
@@ -18,97 +18,116 @@ function Square(props) {
       this.state = {
         squares: Array(9).fill(null),
         xIsNext: true,
-        timer: false,
-        time: 5
+        timer: null,
+        timeLeft: 5
       };
       
     }
 
-    handleClick(i){
-      const squares = this.state.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-      squares[i] = this.state.xIsNext ? 'X' : 'О';
-      this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-      
-    });
-    }
+    startTimer() {
+      let timer = setInterval(()=>{
+                var timeLeft = this.state.timeLeft-1
+                if (timeLeft === 0){
+                  clearInterval(timer)
+                }
+                this.setState({
+                  timeLeft: timeLeft
+                })
+              },1000)
+              return this.setState({timeLeft: timeLeft ,time: timer})
+            }
+    
 
-    handleReset = () =>{
-      this.setState(state =>({
-        squares: Array(9).fill(null),
-        xIsNext: true,
-        timer: null,
+    // handleClick(i){
+    //   const squares = this.state.squares.slice();
+    //   if (calculateWinner(squares) || squares[i]) {
+    //     this.startTimer
+    //     return
+    //   }
+    //   squares[i] = this.state.xIsNext ? 'X' : 'О';
+    //   this.setState({
+    //   squares: squares,
+    //   xIsNext: !this.state.xIsNext,
+      
+    // });
+    // }
+
+    // handleReset = () =>{
+    //   this.setState(state =>({
+    //     squares: Array(9).fill(null),
+    //     xIsNext: true,
+    //     timer: null,
     
       
   
-      }));
+    //   }));
 
-    }
+    // }
     
-    renderSquare(i) {
-      return (
-        <Square 
-          value={this.state.squares[i]} 
-          onClick={() => this.handleClick(i)} // не делать
-          />
-        );
+    // renderSquare(i) {
+    //   return (
+    //     <Square 
+    //       value={this.state.squares[i]} 
+    //       onClick={() => this.handleClick(i)} // не делать
+    //       />
+    //     );
 
-    }
+    // }
     
-    startTimer() {
-          
-          let timer = setInterval(()=>{
-            var timeLeft = this.state.time-1
+    // startTimer() {
+    //       
+    //       let timer = setInterval(()=>{
+    //         var timeLeft = this.state.time-1
       
-            if (timeLeft === 0){
-              clearInterval(timer)
-            }
-            this.setState({
-              time: timeLeft
-            })
-          },1000)
-          return this.setState({time: time})
-        }
+    //         if (timeLeft === 0){
+    //           clearInterval(timer)
+    //         }
+    //         this.setState({
+    //           time: timeLeft
+    //         })
+    //       },1000)
+    //       return this.setState({time: timer})
+    //     }
 
 
     render() {
-      const winner = calculateWinner(this.state.squares);
-      const {timer} = this.state
-      let status;
-      if (winner){
-        this.startTimer()
-        status = 'Выиграл ' + winner
+      // const winner = calculateWinner(this.state.squares);
+      // //const {timer} = this.state
+      // let status;
+      // if (winner){
+      //   this.startTimer()
+      //   status = 'Выиграл ' + winner
 
-        if (!timer) { this.setState({ timer: true })}
-      } else {
-      status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
+        
+      // } else {
+      // status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+      // }
       return (
+        // <div>
+        //   <div className="status">{status}</div>
+        //   <div className="board-row">
+        //     {this.renderSquare(0)}
+        //     {this.renderSquare(1)}
+        //     {this.renderSquare(2)}
+        //   </div>
+        //   <div className="board-row">
+        //     {this.renderSquare(3)}
+        //     {this.renderSquare(4)}
+        //     {this.renderSquare(5)}
+        //   </div>
+        //   <div className="board-row">
+        //     {this.renderSquare(6)}
+        //     {this.renderSquare(7)}
+        //     {this.renderSquare(8)}
+        //   </div>
+        //    <div>
+          
+        //   <h1>Game start: {this.state.timer}</h1>
+        //   </div> 
+        // </div>
         <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-          <div>
-          
-          
-          </div>
+            <h1>Time to: {this.state.time} </h1>
+           <div> <button onClick = {this.startTimer}>timerGo</button></div>
         </div>
       );
     }
@@ -121,9 +140,9 @@ function Square(props) {
           <div className="game-board">
             <Board />
           </div>
-          <div>
+          {/* <div>
           <TimerDisplay />
-          </div>
+          </div> */}
           <div className="game-info">
             <div>{/* status */}</div>
             <ol>{/* TODO */}</ol>
@@ -132,15 +151,15 @@ function Square(props) {
       );
     }
   }
- class TimerDisplay extends React.Component {
-   render() {
-      if(this.state.time === 0 || this.state.time === null){
-          return <div></div>
-      }
-          return <h1>Game started: {this.state.time}</h1>
+//  class TimerDisplay extends React.Component {
+//    render() {
+//       if(this.props.time === 0 || this.props.time === null){
+//           return <div></div>
+//       }
+//           return <h1>Game starts in: {this.props.time}</h1>
       
-   }
- }
+//    }
+//  }
 
   
   
@@ -151,22 +170,22 @@ function Square(props) {
     document.getElementById('root')
   );
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
+  // function calculateWinner(squares) {
+  //   const lines = [
+  //     [0, 1, 2],
+  //     [3, 4, 5],
+  //     [6, 7, 8],
+  //     [0, 3, 6],
+  //     [1, 4, 7],
+  //     [2, 5, 8],
+  //     [0, 4, 8],
+  //     [2, 4, 6],
+  //   ];
+  //   for (let i = 0; i < lines.length; i++) {
+  //     const [a, b, c] = lines[i];
+  //     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+  //       return squares[a];
+  //     }
+  //   }
+  //   return null;
+  // }
