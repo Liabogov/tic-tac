83ad06836264a78b4dc690abff7aed53c3691b77
Богдан Лябогов <bogdanlabogov@MacBook-Pro-Bogdan.local>
2020-04-03 18:23:02 +0300

    Cool version

--- src/index.js
+++ src/index.js
@@ -1,209 +1,162 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 
-function Square(props) {
-      return (
-        <button className="square" onClick={props.onClick}>
-          {props.value}
-        </button>
-      );
-    }
-    
+const Square = ({ onClick, value }) => (
+  <button className="square" onClick={onClick}>
+    {value}
+  </button>
+);
+
+class Board extends React.Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+    isGameOver: false,
+    isDraw: false,
+    timeLeft: 20,
+  }
   
-  
-  class Board extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-        isGameOver: false,
-        timeLeft: null
-      };
-      
+
+  handleClick = (i) => {
+    const { xIsNext } = this.state;
+    const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) { return; }
+
+    squares[i] = xIsNext ? 'X' : 'О';
+
+    if (areAllBoxesClicked(squares)) {
+      this.setState({ isGameOver: true, squares, isDraw: true });
+      this.startTimer();
+
+      return;
     }
 
+    if (calculateWinner(squares)) {
+      this.setState({ isGameOver: true, squares });
+      this.startTimer();
 
-
-    handleClick(i){
-      //const {isGameOver} = this.state;
-      const squares = this.state.squares.slice();
-      //if (!isGameOver) { this.setState({ isGameOver: true }) }
-      if (calculateWinner(squares) || squares[i]) {
-        return
-      }
-      squares[i] = this.state.xIsNext ? 'X' : 'О';
-      this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-      timeLeft: 5
-      
-    });
+      return;
     }
 
-    handleReset = () =>{
-      this.setState(state =>({
-        squares: Array(9).fill(null),
-        xIsNext: true,
-        timer: null,
-    
-      
-  
-      }));
-
-    }
-    
-    renderSquare(i) {
-      return (
-        <Square 
-          value={this.state.squares[i]} 
-          onClick={() => this.handleClick(i)} // не делать
-          />
-        );
-
-    }
-    
-    // startTimer() {
-    //       
-    //       let timer = setInterval(()=>{
-    //         var timeLeft = this.state.time-1
-      
-    //         if (timeLeft === 0){
-    //           clearInterval(timer)
-    //         }
-    //         this.setState({
-    //           time: timeLeft
-    //         })
-    //       },1000)
-    //       return this.setState({time: timer})
-    //     }
-
-
-    componentDidMount(){
-      this.timerID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-    // componentWillMount(){
-    //   clearInterval(this.timerID);
-    // }
-
-    tick() {
-      var newTime = this.state.timeLeft - 1
-      if (newTime === 0){
-                  clearInterval(this.timerID)
-          // this.setState({
-          //   isGameOver: false
-          // });
-          this.setState({
-            squares: Array(9).fill(null),
-            xIsNext: true,
-            isGameOver: false,
-          });        
-        }
-      this.setState({
-        timeLeft: newTime
-      });
-    }
-
-
-    render() {
-      const {isGameOver} = this.state;
-      const winner = calculateWinner(this.state.squares);
-      //const {timer} = this.state
-      let status;
-      if (winner){
-        status = 'Выиграл ' + winner
-        if (!isGameOver) { this.setState({ isGameOver: true }) }
-      
-        
-      } else {
-      status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-           <div>
-          
-          { isGameOver && <h1>Game started in: {this.state.timeLeft} </h1>}
-          </div> 
-        </div>
-        
-      );
-    }
+    this.setState({ squares, xIsNext: !xIsNext });
   }
 
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          {/* <div>
-          <TimerDisplay />
-          </div> */}
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-//  class TimerDisplay extends React.Component {
-//    render() {
-//       if(this.props.timeLeft === 0 || this.props.timeLeft === null){
-//           return <div></div>
-//       }
-//           return <h1>Game starts in: {this.props.timeLeft}</h1>
-      
-//    }
-//  }
-
-  
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
+  renderSquare = (i) => (
+    <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />
   );
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+  
+  startTimer = () => {
+    this.interval = setInterval(() => {
+      if (this.state.timeLeft > 0) {
+        this.setState({ timeLeft: this.state.timeLeft - 1 });
+      } else {
+        this.handleReset();
+        clearInterval(this.interval);
       }
-    }
-    return null;
+    }, 1000);
+  };
+
+  handleReset = () => {
+    clearInterval(this.interval)
+    this.setState({
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      isGameOver: false,
+      isDraw: false,
+      timeLeft: 20,
+    });
   }
+
+  render = () => {
+    const { isGameOver } = this.state;
+    const winner = calculateWinner(this.state.squares);
+    const isDraw = areAllBoxesClicked(this.state.squares);
+
+    let status;
+    if (isDraw) {
+      status = 'Draw';
+    } else if (winner) {
+      status = 'Выиграл ' + winner;
+    } else {
+      status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div>
+          {isGameOver && <button onClick = {this.handleReset}>Reset Game</button>}
+        </div>
+        <div>
+          {isGameOver && <h1>Game started in: {this.state.timeLeft}</h1>}
+        </div>
+      </div>
+    );
+  }
+}
+
+const Game = () => (
+  <div className="game">
+    <div className="game-board">
+      <Board />
+    </div>
+    <div className="game-info">
+      <div>{/* status */}</div>
+      <ol>{/* TODO */}</ol>
+    </div>
+  </div>
+);
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  let winner = null;
+  lines.forEach((_, index) => {
+    const [a, b, c] = lines[index];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      winner = squares[a];
+    }
+  });
+
+  return winner;
+};
+
+const areAllBoxesClicked = (squares) => {
+  let count = 0;
+
+  squares.forEach(item => item && count++);
+
+  return count === 9;
+};
+
+ReactDOM.render(<Game />, document.getElementById('root'));
